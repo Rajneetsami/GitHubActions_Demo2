f8eac25c1aab91e830bcf7366a1bb7c6a9d29171
Rajneet kaur <126429338+Rajneetsami@users.noreply.github.com>
2024-11-08 11:17:59 +0100

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-from flask import Flask
 
-app = Flask(__name__)
+def add(a, b):
+    return a + b
 
-@app.route('/')
-def home():
-    return "Hello, Azuresssssssssssssss!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+def subtract(a, b):
+    return a - b
