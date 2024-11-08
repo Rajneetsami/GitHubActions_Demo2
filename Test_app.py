519adff22069f9b391518cb5b1e4858a398edbf6
Rajneet kaur <126429338+Rajneetsami@users.noreply.github.com>
2024-11-08 11:17:34 +0100

    Test_app.py

--- Test_app.py
+++ Test_app.py
@@ -1,7 +1,10 @@
 
+# test_app.py
+
+from app import add, subtract
 
 def test_addition():
-    assert 1 + 1 == 2
+    assert add(1, 1) == 2
 
 def test_subtraction():
-    assert 5 - 3 == 2
+    assert subtract(5, 3) == 2
